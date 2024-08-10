8528a778c276364c0d559dd6af96a889480dcd76
RK <r.raajey@gmail.com>
2024-08-10 22:45:35 +0530

    Update exprimental snippet

--- crates/testing/tests/tests_0/prime_v1.rs
+++ crates/testing/tests/tests_0/prime_v1.rs
@@ -1,35 +1,38 @@
 use std::{sync::Arc, time::Duration};
 
+use async_compatibility_layer::art::async_timeout;
 use futures::StreamExt;
 use hotshot::tasks::task_state::CreateTaskState;
 use hotshot_example_types::{
-    block_types::{TestMetadata, TestTransaction},
+    block_types::TestTransaction,
     node_types::{MemoryImpl, TestTypes},
 };
 use hotshot_macros::{run_test, test_scripts};
-use hotshot_task_impls::{da::DaTaskState, events::HotShotEvent::*};
+use hotshot_task_impls::{da::DaTaskState, events::HotShotEvent};
 use hotshot_testing::{
     helpers::build_system_handle_from_launcher,
-    predicates::event::exact,
+    predicates::{
+        event::{exact, expect_external_events, ext_event_exact},
+        PredicateResult,
+    },
     script::{Expectations, InputOrder, TaskScript},
     serial,
     test_builder::TestDescription,
     view_generator::TestViewGenerator,
 };
 use hotshot_types::{
-    constants::BaseVersion,
-    data::{null_block, PackedBundle, ViewNumber},
+    data::ViewNumber,
+    event::{Event, EventType},
     simple_vote::DaData,
     traits::{
         block_contents::precompute_vid_commitment, election::Membership,
         node_implementation::ConsensusTime,
     },
 };
-use vbs::version::StaticVersionType;
 
 #[cfg_attr(async_executor_impl = "tokio", tokio::test(flavor = "multi_thread"))]
 #[cfg_attr(async_executor_impl = "async-std", async_std::test)]
-async fn test_da_task_with_custom_config() {
+async fn test_da_task_outdated_proposal() {
     async_compatibility_layer::logging::setup_logging();
     async_compatibility_layer::logging::setup_backtrace();
 
@@ -41,7 +44,6 @@ async fn test_da_task_with_custom_config() {
     test_description.num_nodes_with_stake = num_nodes;
     test_description.da_staked_committee_size = da_committee_size;
     test_description.start_nodes = num_nodes;
-    // Add more customizations as needed
 
     let launcher = test_description
         .gen_launcher(node_id)
@@ -49,7 +51,6 @@ async fn test_da_task_with_custom_config() {
             config.next_view_timeout = 1000;
             config.timeout_ratio = (12, 10);
             config.da_staked_committee_size = da_committee_size;
-            // Modify other config parameters as needed
         });
 
     let (handle, _event_sender, _event_receiver) =
@@ -61,7 +62,7 @@ async fn test_da_task_with_custom_config() {
     // later calls. We need the VID commitment to be able to propose later.
     let transactions = vec![TestTransaction::new(vec![0])];
     let encoded_transactions = Arc::from(TestTransaction::encode(&transactions));
-    let (payload_commit, precompute) = precompute_vid_commitment(
+    let (payload_commit, _precompute) = precompute_vid_commitment(
         &encoded_transactions,
         handle.hotshot.memberships.quorum_membership.total_nodes(),
     );
@@ -71,66 +72,52 @@ async fn test_da_task_with_custom_config() {
         handle.hotshot.memberships.da_membership.clone(),
     );
 
-    let mut proposals = Vec::new();
-    let mut leaders = Vec::new();
-    let mut votes = Vec::new();
-    let mut dacs = Vec::new();
-    let mut vids = Vec::new();
+    // Generate views
+    let view1 = view_generator.next().await.unwrap();
+    let _view2 = view_generator.next().await.unwrap();
 
-    for view in (&mut view_generator).take(1).collect::<Vec<_>>().await {
-        proposals.push(view.da_proposal.clone());
-        leaders.push(view.leader_public_key);
-        votes.push(view.create_da_vote(DaData { payload_commit }, &handle));
-        dacs.push(view.da_certificate.clone());
-        vids.push(view.vid_proposal.clone());
-    }
+    view_generator.add_transactions(transactions);
 
-    view_generator.add_transactions(vec![TestTransaction::new(vec![0])]);
+    let view3 = view_generator.next().await.unwrap();
 
-    for view in (&mut view_generator).take(1).collect::<Vec<_>>().await {
-        proposals.push(view.da_proposal.clone());
-        leaders.push(view.leader_public_key);
-        votes.push(view.create_da_vote(DaData { payload_commit }, &handle));
-        dacs.push(view.da_certificate.clone());
-        vids.push(view.vid_proposal.clone());
-    }
-
-    // Generate test inputs & expectations
+    // Prepare inputs
     let inputs = vec![
         serial![
-            ViewChange(ViewNumber::new(1)),
-            ViewChange(ViewNumber::new(2)),
-            BlockRecv(PackedBundle::new(
-                encoded_transactions,
-                TestMetadata,
-                ViewNumber::new(2),
-                vec1::vec1![null_block::builder_fee(
-                    handle
-                        .hotshot
-                        .memberships
-                        .quorum_membership
-                        .clone()
-                        .total_nodes(),
-                    BaseVersion::version()
-                )
-                .unwrap()],
-                Some(precompute),
-                None,
-            )),
+            HotShotEvent::ViewChange(ViewNumber::new(1)),
+            HotShotEvent::ViewChange(ViewNumber::new(2)),
+            HotShotEvent::ViewChange(ViewNumber::new(3)),
+            // Send an outdated proposal (view 1) when we're in view 3
+            HotShotEvent::DaProposalRecv(view1.da_proposal.clone(), view1.leader_public_key),
         ],
-        serial![DaProposalRecv(proposals[1].clone(), leaders[1])],
+        serial![
+            // Send a current proposal (view 3)
+            HotShotEvent::DaProposalRecv(view3.da_proposal.clone(), view3.leader_public_key),
+        ],
     ];
 
     let expectations = vec![
-        Expectations::from_outputs(vec![exact(DaProposalSend(
-            proposals[1].clone(),
-            leaders[1],
-        ))]),
+        Expectations::from_outputs(vec![]), // Expect no output for the outdated proposal
         Expectations::from_outputs(vec![
-            exact(DaProposalValidated(proposals[1].clone(), leaders[1])),
-            exact(DaVoteSend(votes[1].clone())),
+            exact(HotShotEvent::DaProposalValidated(
+                view3.da_proposal.clone(),
+                view3.leader_public_key,
+            )),
+            exact(HotShotEvent::DaVoteSend(view3.create_da_vote(
+                DaData {
+                    payload_commit: payload_commit,
+                },
+                &handle,
+            ))),
         ]),
     ];
+
+    let external_event_expectations = vec![expect_external_events(vec![ext_event_exact(Event {
+        view_number: view3.view_number,
+        event: EventType::DaProposal {
+            proposal: view3.da_proposal.clone(),
+            sender: view3.leader_public_key,
+        },
+    })])];
 
     let da_state = DaTaskState::<TestTypes, MemoryImpl>::create_from(&handle).await;
     let mut da_script = TaskScript {
@@ -139,5 +126,42 @@ async fn test_da_task_with_custom_config() {
         expectations: expectations,
     };
 
+    let mut output_event_stream_recv = handle.event_stream();
+
     run_test![inputs, da_script].await;
+
+    // Check external events
+    let mut external_event_expectations_met = vec![false; external_event_expectations.len()];
+
+    while let Ok(Some(ext_event_received_output)) =
+        async_timeout(da_script.timeout, output_event_stream_recv.next()).await
+    {
+        tracing::debug!("Test received Ext Event: {:?}", ext_event_received_output);
+
+        for (index, expectation) in external_event_expectations.iter().enumerate() {
+            if !external_event_expectations_met[index] {
+                for predicate in &expectation.output_asserts {
+                    if predicate
+                        .evaluate(&Arc::new(ext_event_received_output.clone()))
+                        .await
+                        == PredicateResult::Pass
+                    {
+                        external_event_expectations_met[index] = true;
+                        break;
+                    }
+                }
+            }
+        }
+
+        // Check if all expectations are met
+        if external_event_expectations_met.iter().all(|&x| x) {
+            break;
+        }
+    }
+
+    // Assert that all external event expectations were met
+    assert!(
+        external_event_expectations_met.iter().all(|&x| x),
+        "Not all external event expectations were met"
+    );
 }
